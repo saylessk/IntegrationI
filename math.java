0dc42a39b12de8493699e8a2a9479a9b5929668a
saylessk <35510336+saylessk@users.noreply.github.com>
2018-04-17 13:01:41 -0400

    Update math.java

--- math.java
+++ math.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class math {
 	
- public void divison(int w) {
+ public void divison() {
    Scanner reader = new Scanner(System.in);
    System.out.println("Enter a number to be cut in half: ");
-   int u = reader.nextInt();
-   int b = u / 2;
-   System.out.println(u + "cut in half equals " + b);
+   int numerator = reader.nextInt();
+   int b = numerator / 2;
+   System.out.println(numerator + "cut in half equals " + b);
    
 	
 	}

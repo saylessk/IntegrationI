e45143a7699909b23e07bdf60c894a884ba0cc4c
saylessk <35510336+saylessk@users.noreply.github.com>
2018-04-26 18:19:35 -0400

    Create LocalTimeExample.java

---
+++ LocalTimeExample.java
@@ -0,0 +1,15 @@
+import java.time.LocalTime;
+
+
+
+public class LocalTimeExample {
+
+	public static void main(String[] args) {
+		
+		//Current Time
+		LocalTime time = LocalTime.now();
+		System.out.println("Current Time="+time + " EST");
+		
+	}
+
+}

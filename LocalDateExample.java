facca4f0a5f3ade3d48e83752661adacb0554d98
saylessk <35510336+saylessk@users.noreply.github.com>
2018-04-26 18:20:46 -0400

    Create LocalDateExample.java

---
+++ LocalDateExample.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+
+
+
+public class LocalDateExample {
+
+	public static void main(String[] args) {
+		
+		//Current Date
+		LocalDate today = LocalDate.now();
+		System.out.println("Current Date="+today);
+		
+		
+	}
+	
+	
+
+}

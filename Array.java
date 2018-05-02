508792e42e3d85b12c1b2a799795a699aeb5c404
saylessk <35510336+saylessk@users.noreply.github.com>
2018-05-01 23:40:40 -0400

    Update Array.java

--- Array.java
+++ Array.java
@@ -25,13 +25,24 @@ public class Array {
 		y = in.nextInt();
 		z = in.nextInt();
 		
-		if (x > y && x > z)
-			System.out.println(x + " is the largest number.");
-		else if (y > x && y > z)
-			System.out.println(y + " is the largest number.");
-		else if (z > x && z > y)
-			System.out.println(z + " is the largest number.");
+		// finds smallest number
+		
+		if (x < y && x < z)
+			System.out.println(x + " is the smallest number.");
+		else if (y < x && y < z)
+			System.out.println(y + " is the smallest number.");
+		else if (z < x && z < y)
+			System.out.println(z + " is the smallest number.");
 	}
 	
-
-}
+	 public static void main2(String[] args) {
+	        String[][] names = {
+	            {"Mr. ", "Mrs. ", "Ms. "},
+	            {"Perry", "Walters"}
+	        };
+	        
+	        System.out.println(names[0][0] + names[1][0]);
+	       
+	        System.out.println(names[0][2] + names[1][1]);
+	    }
+	}
